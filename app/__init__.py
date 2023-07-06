2d97c9c34e82e55a5f8c927001b42ff2f1144b00
Michael Zhan <michaelzhan2001@gmail.com>
2023-07-06 16:12:47 -0500

    Set up database

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.config.from_object('config.Config')
 
 
-from app import routes
+from app import routes, models
--- app/models.py
+++ app/models.py
@@ -4,4 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
 
-# TODO: continue with database implementation
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+
+with app.app_context():
+    db.create_all()
