be28e40b0b16ae5b4442b879fa1cbe542aa4f98a
Michael Zhan <michaelzhan2001@gmail.com>
2023-07-06 16:44:06 -0500

    Update db schema

--- app/models.py
+++ app/models.py
@@ -9,5 +9,13 @@ class People(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
 
 
+class Payments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    payer = db.Column(db.String(50))
+    payer_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+    payees = db.Column(db.String, nullable=False)   # json string
+
+
 with app.app_context():
     db.create_all()
