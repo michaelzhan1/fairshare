c60abd0b9ef8b25010b03b721fbf4d1521a406f7
Michael Zhan <michaelzhan2001@gmail.com>
2023-07-07 11:36:52 -0500

    Make add payment popup dynamic

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -11,16 +11,65 @@ document.addEventListener('DOMContentLoaded', function() {
   const addPaymentForm = document.getElementById('add-payment-form');
   const addPersonForm = document.getElementById('add-person-form');
 
+  // payment form inputs
+  const chooseInvolved = document.getElementById('choose-involved');
+  const choosePayer = document.getElementById('choose-payer-select');
+
+
+  // functions
+  async function getPeople() {
+    let response = await fetch('/get_people');
+    let data = await response.json();
+    return data.names;
+  }
+
 
   // Event listeners
   // Open forms
   newPaymentBtn.addEventListener('click', function() {
     addPaymentFormContainer.style.display = 'block';
+
+    // Populate involved people
+    getPeople().then(people => {
+      chooseInvolved.innerHTML = '<legend>Select people involved in payment:</legend>';
+      let i = 0;
+      people.forEach(person => {
+        let option = document.createElement('input');
+        option.type = 'checkbox';
+        option.value = person;
+        option.name = 'involved';
+        option.id = `person${i++}`;
+        chooseInvolved.appendChild(option);
+
+        let label = document.createElement('label');
+        label.htmlFor = option.id;
+        label.textContent = person;
+        chooseInvolved.appendChild(label);
+
+        let br = document.createElement('br');
+        chooseInvolved.appendChild(br);
+      });
+    });
   });
 
+  // Open person add form
   newPersonBtn.addEventListener('click', function() {
     addPersonFormContainer.style.display = 'block';
   });
+
+  // Update payer select dropdown on selecting people involved
+  chooseInvolved.addEventListener('change', function() {
+    choosePayer.innerHTML = '<option disabled selected>--</option>';
+
+    let involved = document.querySelectorAll('input[name="involved"]:checked');
+    involved.forEach(person => {
+      let option = document.createElement('option');
+      option.value = person.value;
+      option.textContent = person.value;
+      choosePayer.appendChild(option);
+    });
+  });
+
 
   // Close forms
   addPaymentFormContainer.addEventListener('click', function(e) {
@@ -36,13 +85,6 @@ document.addEventListener('DOMContentLoaded', function() {
       addPersonFormContainer.style.display = 'none';
     }
   });
-
-
-  async function getPeople() {
-    let response = await fetch('/get_people');
-    let data = await response.json();
-    return data.names;
-  }
 
 
   // Validate person submission
