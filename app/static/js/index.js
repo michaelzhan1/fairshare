98cd9552360bba1e52915f319899170be8901c4a
Michael Zhan <michaelzhan2001@gmail.com>
2023-07-12 12:02:47 -0500

    Add main page functionality

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,2 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const groupbutton = document.querySelector('#group-id-submit');
+  groupbutton.addEventListener('click', function() {
+    const groupid = document.querySelector('#group-id-input').value;
+    window.location.href = '/g/' + groupid;
+  });
 });
