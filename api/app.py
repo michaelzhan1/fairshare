b57f565a88de23a5eed7335080cb90197c8e2953
Michael Zhan <michaelzhan2001@gmail.com>
2023-07-12 20:14:53 -0500

    Edit structure

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expensetrack.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['FLASK_ENV'] = 'production'
